519322059eea371c2e1fbd33b5d7224255eae8a1
patmozdz <123921845+patmozdz@users.noreply.github.com>
2023-12-19 19:50:42 -0600

    Add chat_test.py and secrets_manager.py and rough draft of steps to Dockerize

---
+++ AutoTasks/reminders/utils.py
@@ -0,0 +1,26 @@
+def create_reminder(user_id, title, description, reminder_time, recurring_interval=None, urgency=None):
+    """
+    Creates a new reminder.
+    """
+    pass
+
+
+def edit_reminder(user_id, reminder_id, title=None, description=None, reminder_time=None, recurring_interval=None, urgency=None):
+    """
+    Edits an existing reminder.
+    """
+    pass
+
+
+def delete_reminder(user_id, reminder_id):
+    """
+    Deletes an existing reminder.
+    """
+    pass
+
+
+def send_notification(user_id, reminder_id):
+    """
+    Sends a notification for a reminder.
+    """
+    pass
