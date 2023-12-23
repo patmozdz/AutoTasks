a5fad92e744e9471d2b21bc835577beebbee83a6
patmozdz <123921845+patmozdz@users.noreply.github.com>
2023-12-22 18:43:11 -0600

    Update settings to install all apps

--- AutoTasks_backend/AutoTasks_backend/settings.py
+++ AutoTasks_backend/AutoTasks_backend/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user',
     'api',
+    'chat',
     'reminders',
+    'user',
 ]
 
 AUTH_USER_MODEL = 'user.User'
