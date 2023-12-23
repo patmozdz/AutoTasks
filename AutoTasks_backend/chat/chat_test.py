443a40e68c67f4dd5e1650dba63d106cd0808249
patmozdz <123921845+patmozdz@users.noreply.github.com>
2023-12-22 18:43:34 -0600

    Completely redo chat_test.py to actually test out system

--- AutoTasks_backend/chat/chat_test.py
+++ AutoTasks_backend/chat/chat_test.py
@@ -1,96 +1,56 @@
-# import the OpenAI Python library for calling the OpenAI API
-from openai import OpenAI
-from AutoTasks_backend.chat.secrets_manager import OPENAI_API_KEY
-from tenacity import retry, wait_random_exponential, stop_after_attempt
+import os
+import django
+import sys
+
+sys.path.append("C:/Users/Papis/Documents/~GitHub Projects/AutoTasks/AutoTasks_backend")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AutoTasks_backend.settings')
+django.setup()
+
+from rest_framework import status
+from django.contrib.auth import authenticate
+from twilio.twiml.messaging_response import MessagingResponse
+from user.models import User
+from chat.models import Chat
 
 
-GPT_MODEL = "gpt-4-vision-preview"
-client = OpenAI(api_key=OPENAI_API_KEY)
-messages_temp = [
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-]
+def pretend_receive_sms_from_twilio(request):
+    body = request.get('Body', None)
+    phone = request.get('From', None)
+
+    if not body or not phone:
+        return {"error": "Invalid data."}, status.HTTP_400_BAD_REQUEST
+
+    user = authenticate(phone=phone)  # Modified to use username=phone
+    if user:
+        end_choices = ['END', 'STOP', 'QUIT', 'EXIT', 'UNSUBSCRIBE', 'CANCEL']
+        if body in end_choices:
+            # TODO: Handle user unsubscribing, maybe delete account?
+            resp = MessagingResponse()
+            resp.message("You have been unsubscribed.")
+        else:
+            # Handle user sending a message
+            resp = MessagingResponse()
+            chat_response = Chat.chat_completion_from_sms(user, body)
+            resp.message = chat_response.choices[0].message.content
+    else:
+        # Handle unauthenticated user
+        new_user = User.objects.create_user(phone=phone)  # Modified to use username=phone
+        new_user.save()
+        resp = MessagingResponse()
+        resp.message("Thank you for registering! Please type to engage with the autotasker.")
+
+    return str(resp), status.HTTP_200_OK
 
 
-@retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-def chat_completion_request(messages: dict, tools=None, tool_choice=None, model=GPT_MODEL):
-    try:
-        # Temporarily while tools are not implemented
-        args = {
-            "messages": messages,
-            "model": model,
-        }
+if __name__ == '__main__':
+    # This is a simulation
+    phone_number = input("Phone number:")
+    while True:
+        simulated_text_message = input("Send sms:")
+        pretend_request = {'Body': simulated_text_message, 'From': phone_number}
 
-        # Conditionally include the 'tools' parameter
-        if tools is not None:
-            args["tools"] = tools
+        if simulated_text_message == "quit":
+            break
 
-        # Call the API method with the arguments
-        response = client.chat.completions.create(**args)
-
-        return response
-
-    except Exception as e:
-        print("Unable to generate ChatCompletion response")
-        print(f"Exception: {e}")
-
-        return e
-
-
-chat_completion = chat_completion_request(messages_temp)
-print(chat_completion)
-
-
-# Example tools:
-# tools = [
-#     {
-#         "type": "function",
-#         "function": {
-#             "name": "get_current_weather",
-#             "description": "Get the current weather",
-#             "parameters": {
-#                 "type": "object",
-#                 "properties": {
-#                     "location": {
-#                         "type": "string",
-#                         "description": "The city and state, e.g. San Francisco, CA",
-#                     },
-#                     "format": {
-#                         "type": "string",
-#                         "enum": ["celsius", "fahrenheit"],
-#                         "description": "The temperature unit to use. Infer this from the users location.",
-#                     },
-#                 },
-#                 "required": ["location", "format"],
-#             },
-#         }
-#     },
-#     {
-#         "type": "function",
-#         "function": {
-#             "name": "get_n_day_weather_forecast",
-#             "description": "Get an N-day weather forecast",
-#             "parameters": {
-#                 "type": "object",
-#                 "properties": {
-#                     "location": {
-#                         "type": "string",
-#                         "description": "The city and state, e.g. San Francisco, CA",
-#                     },
-#                     "format": {
-#                         "type": "string",
-#                         "enum": ["celsius", "fahrenheit"],
-#                         "description": "The temperature unit to use. Infer this from the users location.",
-#                     },
-#                     "num_days": {
-#                         "type": "integer",
-#                         "description": "The number of days to forecast",
-#                     }
-#                 },
-#                 "required": ["location", "format", "num_days"]
-#             },
-#         }
-#     },
-# ]
+        response, status_code = pretend_receive_sms_from_twilio(pretend_request)
+        print(response)
